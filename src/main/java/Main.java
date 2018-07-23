47c0e0b213c1cce35958dd8efcd7784a9fa1aadd
Unknown <iamtravisw@gmail.com>
2018-07-23 05:54:47 +0000

    GET and POST Responses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,36 @@
 import static spark.Spark.*;
 import java.sql.*;
-
-import java.sql.Timestamp;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        get("/input/", (request, response) -> {
+
+        get("/input", (request, response) -> {
+            return "Hello: " + request.params(":email");
+        });
+
+        get("/goodmorning/public/index.html", (request, response) -> {
             return "Hello: " + request.params(":name");
         });
+
+        post("/input", (request, response) -> {
+            String a, b;
+            a = request.queryParams("name");
+            b = request.queryParams("email");
+            return String.join(" AND ", a, b);
+        });
+
     }
 
     public static void sendData(String[] args) {
 
-
-
+        String name = "";
         String email = "";
         String subscribed = "Y";
         Date adddate = new Date();
         Date moddate = new Date();
-
 
         Connection conn = null;
         try {
@@ -37,13 +46,14 @@ public class Main {
             System.out.println("Inserting...");
 
             // insert statement
-            String query = "insert into CharacterBasics (Email, Subscribed, AddDate, ModDate)"
-                    + "values (?, ?, ?, ?)";
+            String query = "insert into CharacterBasics (Name, Email, Subscribed, AddDate, ModDate)"
+                    + "values (?, ?, ?, ?, ?)";
             PreparedStatement insertUsers = conn.prepareStatement(query);
-            insertUsers.setString(1, email);
-            insertUsers.setString(2, subscribed);
-            insertUsers.setString(3, adddate.toString());
-            insertUsers.setString(4, moddate.toString());
+            insertUsers.setString(1, name);
+            insertUsers.setString(2, email);
+            insertUsers.setString(3, subscribed);
+            insertUsers.setString(4, adddate.toString());
+            insertUsers.setString(5, moddate.toString());
             insertUsers.execute(); // execute
 
             System.out.println("Succesfully added");
