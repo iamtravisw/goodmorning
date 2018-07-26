6d338b0581882c9cac1c783f6f54c818c9f15d77
Unknown <iamtravisw@gmail.com>
2018-07-26 05:18:05 +0000

    setup database connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,84 +1,90 @@
-
 import java.sql.*;
 import java.util.Date;
 import static spark.Spark.*;
-
-
 import java.util.HashMap;
 import java.util.Map;
-
 import spark.ModelAndView;
-import spark.Spark;
-
 import static spark.Spark.get;
 import spark.template.velocity.*;
-
-// Error:(14, 31) java: package spark.template.velocity does not exist
-
 
 public class Main {
 
     public static void main(String[] args) {
 
-        staticFiles.location("/public");
-
-        get("/", (rq, rs) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "templates/index.vm");
-        });
-
-        // route to user subscribing
-        post("/input", (request, response) -> {
-            String a, b;
-            a = request.queryParams("name");
-            b = request.queryParams("email");
-            return String.join(" AND ", a, b);
-        });
-    }
-
-    public static void sendData(String[] args) {
-
-        String name = "";
-        String email = "";
+        // For MySQL Insert
         String subscribed = "Y";
         Date adddate = new Date();
         Date moddate = new Date();
 
-        Connection conn = null;
-        try {
-            // connect to database
-            String url = "jdbc:mysql://localhost:3306/GoodMorning";
-            String user = "root";
-            String password = "Pepper";
+        staticFiles.location("/public");
+        redirect.post("/input", "/thanks"); // Never let the end user see the post
 
-            conn = DriverManager.getConnection(url, user, password);
-            if (conn != null) {
-            }
+            // this is my home page
+            get("/", (rq, rs) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "templates/index.vm");
+            });
 
-            System.out.println("Inserting...");
+            // when someone subs, take them here
+            get("/thanks", (rq, rs) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "templates/thanks.vm");
+            });
 
-            // insert statement
-            String query = "insert into Users (Name, Email, Subscribed, AddDate, ModDate)"
-                    + "values (?, ?, ?, ?, ?)";
-            PreparedStatement insertUsers = conn.prepareStatement(query);
-            insertUsers.setString(1, name);
-            insertUsers.setString(2, email);
-            insertUsers.setString(3, subscribed);
-            insertUsers.setString(4, adddate.toString());
-            insertUsers.setString(5, moddate.toString());
-            insertUsers.execute(); // execute
+            // when someone unsubs, take them here
+            get("/bye", (rq, rs) -> {
+                Map<String, Object> model = new HashMap<>();
+                return render(model, "templates/bye.vm");
+            });
 
-            System.out.println("Succesfully added");
+            // get name and email from user input
+            post("/input", (request, response) -> {
+                String a, b;
+                a = request.queryParams("name");
+                b = request.queryParams("email");
 
-            conn.close(); // close the connection
-            }
-            catch (Exception e)
-            {
-            System.err.println("Got an exception!");
-            System.err.println(e.getMessage());
-        }
+                if(a != null){
+                    System.out.println(a);
+
+                    try {
+                        // connect to database
+                        final String url = "jdbc:mysql://localhost:3306/GoodMorning";
+                        final String user = "root";
+                        final String password = "Pepper";
+
+                        final Connection conn = DriverManager.getConnection(url, user, password);
+                        if (conn != null) {
+                        }
+                        // sql insert statement
+                        final String query = "insert into Users (Name, Email, Subscribed, AddDate, ModDate)"
+                                + "values (?, ?, ?, ?, ?)";
+                        final PreparedStatement insertUsers = conn.prepareStatement(query);
+
+                        // sql insert values
+                        insertUsers.setString(1, a);
+                        insertUsers.setString(2, b);
+                        insertUsers.setString(3, subscribed);
+                        insertUsers.setString(4, adddate.toString());
+                        insertUsers.setString(5, moddate.toString());
+                        insertUsers.execute(); // execute
+
+                        System.out.println("Succesfully added");
+
+                        conn.close(); // close the connection
+                        System.out.println("DB Connection has been closed.");
+
+                    } catch (Exception e) {
+                        System.err.println("Got an exception!");
+                        System.err.println(e.getMessage());
+
+                    }
+                }else {
+                    System.out.println("Waiting...");
+                }
+               return String.join(" / ", a, b);
+            });
+        System.out.println("main done");
     }
-
     public static String render(Map<String, Object> model, String templatePath) {
         return new VelocityTemplateEngine().render(new ModelAndView(model, templatePath));
     }
